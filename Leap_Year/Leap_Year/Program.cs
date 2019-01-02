70eb7f1b98a2406f6a7cd91a5288a5a129e37475
Abhiruchi55 <46316922+Abhiruchi55@users.noreply.github.com>
2019-01-02 16:15:22 +0530

    Add files via upload

---
+++ Leap_Year/Leap_Year/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leap_Year
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Year YearObj = new Year();
+            YearObj.YearInput();
+        }
+    }
+}
+
---
+++ Leap_Year/Leap_Year/Year.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leap_Year
+{
+    class Year
+    {
+        private int Y;
+
+        public void YearInput()
+        {
+            bool Check;
+            Console.WriteLine("Test Data: ");
+            Y = Convert.ToInt32(Console.ReadLine());
+            Check = YearCheck(Y);
+            if (Check == true)
+                Console.WriteLine(Y + " is a leap year");
+            else
+                Console.WriteLine(Y + " is not a leap year");
+            Console.ReadLine();
+        }
+        public bool YearCheck(int y)
+        {
+            if (y % 4 == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+
+}
